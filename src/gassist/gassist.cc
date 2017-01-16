b215d0f85bfa48395b67908ac816071b76d8eca8
Karolin Varner <karo@cupdev.net>
2017-01-16 19:35:29 +0100

    Outsourse some data into util

--- src/gassist/gassist.cc
+++ src/gassist/gassist.cc
@@ -5,11 +5,9 @@
 
 #include <epoxy/gl.h>
 
-#include <glm/gtc/type_precision.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/constants.hpp>
-
 #include <softwear/thread_pool.hpp>
+
+#include "gassist/util.hh"
 
 #include "gassist/wrap_glfw.hh"
 #include "gassist/wrap_gl.hh"
@@ -18,105 +16,11 @@
 
 using namespace gassist;
 
-
-// DEFAULT TYPES //////////////
-
-typedef glm::mat4  mat4;
-typedef glm::fvec2 vec2;
-typedef glm::fvec3 vec3;
-typedef glm::fvec4 vec4;
-
-
-// CONSTANTS ///////////////////
-
-const float pi  = glm::pi<float>(),
-            tau = 2*pi;
-mat4 identity{1.0f};
-
-
-// HELPERS /////////////////////
-
-/// Generates a translation matrix based on a vector
-mat4 translate(const vec3 &v) {
-  return glm::translate(identity, v);
-}
-
-/// Generates a rotation transform matrix based on an angle
-/// and a rotation axis
-mat4 rotate(const float angle, const vec3 &axis) {
-  return glm::rotate(identity, angle, axis);
-}
-
-/// Applies a 4d matrix transformation to a 3d vector
-/// (converts the 3d vector to 4d by setting w=1 and
-/// then drops w again)
-vec3 operator*(const mat4 &m, const vec3 &v) {
-  auto r = (m * vec4{v, 1});
-  return {r.x, r.y, r.z};
-}
-
-
-// DATA STRUCTURES //////////////////
-
-/// Position vector (normal Cartesian coordinates)
-///
-/// Specializable template; the default implementation
-/// just uses x.pos()
-///
-/// The return value must support conversion to and
-/// assignment from vec3 (like a vec3&)
-template<typename T>
-vec3& pos(T &x) {
-  return x.pos();
-}
-
-
-/// Orientation; what the object is facing towards
-/// relatively to itself. (e.g. (0, 1, 0) would be
-/// facing upwards)
-///
-/// Specializable template; the default implementation
-/// just uses x.focus().
-///
-/// The return value must support conversion to and
-/// assignment from vec3 (like a vec3&)
-template<typename T>
-vec3& focus(T &x) {
-  return x.focus();
-}
-
-/// How the world is rotated; e.g. focus=(0, 0, -1)
-/// roll=0; would be facing front as normal, while
-/// focus=(0, 0, -1) roll=pi would be facing front
-/// with the camera standing on it's head
-///
-/// Specializable template; the default implementation
-/// just uses x.roll()
-///
-/// The return value must support conversion to and
-/// assignment from float (like a float&)
-template<typename T>
-float& roll(T &x) {
-  return x.roll();
-}
-
-/// Just a simple struct that holds position, focus
-/// and roll of any object
-struct location {
-  vec3 pos_;
-  vec3 focus_;
-  float roll_;
-
-  vec3& pos()   { return pos_; }
-  vec3& focus() { return focus_; }
-  float& roll() { return roll_; }
-};
-
 gl::mesh make_triangle() {
   std::array<float, 9> verts{
-    -1, -1, 0,
-     1, -1, 0,
-     0,  4, 0};
+    -1, -1, -1,
+    -1, -1, +1,
+    -1, +1, +1};
 
   return gl::mesh{verts};
 }
@@ -170,9 +74,6 @@ void draw_thr(shared_state &s) {
   // We should have something nicer for this.
   // Window should implicitly create the context
   // and allow it to be used from another thread
-  std::cerr << "pi=" << pi << " fov=" << s.fov
-    << " wx=" << s.win_size.x << " wy=" << s.win_size.y
-    << std::endl;
   s.win.make_gl_context();
 
   gl::program default_prog = asset::load_gl_program("default_prog");
